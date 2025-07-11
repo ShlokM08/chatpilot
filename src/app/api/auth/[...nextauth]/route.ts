745610f77010eba99fea14c966969b55eec197a8
ShlokM08 <shlok21421@iiitd.ac.in>
2025-07-11 23:30:28 +0300

    layout confimration

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,28 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+export const authOptions={
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            
+
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    session:{strategy:"jwt" as const},
+    callbacks:{
+        async jwt({ token, account }: { token: any; account: any }) {
+            if(account)token.accesstoken=account.access_token;
+            return token;
+        },
+        async session({session,token}: { session: any; token: any }){
+            session.accesstoken=token.accesstoken as string;
+            return session;
+        },
+    },
+};
+
+const handler =NextAuth(authOptions);
+export {handler as  GET, handler as POST};
